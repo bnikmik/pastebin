00242357c986d54a9d050c9a5df218924dcda77a
Nikita Bolshakov <bnikmik@gmail.com>
2023-04-14 15:50:39 +0300

    donee

--- src/test/java/com/example/pastebin/PastebinApplicationTests.java
+++ src/test/java/com/example/pastebin/PastebinApplicationTests.java
@@ -3,11 +3,9 @@ package com.example.pastebin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestComponent
 class PastebinApplicationTests {
 
     @Test
